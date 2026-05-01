4dd69f022fb7f335ce5f5cd24361854943ee73a8
needakefir <interesnoyou99@gmail.com>
2026-05-02 02:13:17 +0300

    Add animation to buttons,fixing errors,adding xml and parsing to it

--- js/Index.js
+++ js/Index.js
@@ -6,12 +6,13 @@ button.addEventListener('click',function(){
             behavior: "smooth", block: "center", inline: "start"
         }
     )
+    comments_label.value=""
 })
 const standardComments=[
     "Добрый день,хочу получить консультацию по групповым тренировкам.",
     "Добрый день,хочу получить консультацию по персональным тренировкам.",
     "Добрый день,хочу получить консультацию по детским секциям."];
-let buttons=document.querySelectorAll('.item-offer button');
+let buttons=document.querySelectorAll('.item-offer .button');
 const comments_label=document.getElementById('comments')
 comments_label.value=""
 buttons.forEach(
@@ -22,8 +23,37 @@ buttons.forEach(
                  block: "center", 
                  inline: "start"}
                 );
-            comments_label.value+=standardComments[index];
+            comments_label.value= "" + standardComments[index];
         });
     }
 )
+async function loadReviews()
+{
+    let div_reviews=document.querySelector('.reviews');
+    const response = await fetch('xml/reviews.xml');
+    const xmlString = await response.text();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+    const reviews=xmlDoc.querySelectorAll('review');
+    reviews.forEach(
+        (review)=>{
+        const review_item=document.createElement('div');
+        review_item.setAttribute('class','review-item');
+        const img=document.createElement('img');
+        const name=document.createElement('p');
+        const stars=document.createElement('p');
+        const text=document.createElement('p');
+        img.setAttribute("src",review.children.item(0).textContent);
+        img.setAttribute('alt',"");
+        name.textContent=review.children.item(1).textContent;
+        stars.textContent=review.children.item(2).textContent;
+        text.textContent=review.children.item(3).textContent;
+        review_item.append(img,name,stars,text);
+        div_reviews.append(review_item);
+        }
+    )
+}
+loadReviews()
+
+
 
