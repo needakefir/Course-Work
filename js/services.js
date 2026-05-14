e35bf652ceaf3d96dc20395a6e6b9883130bc9f7
needakefir <interesnoyou99@gmail.com>
2026-05-14 10:29:25 +0300

    Adding animations to services.html,redact scss of services page,correcting mistake of mixin in index.scss

--- js/services.js
+++ js/services.js
@@ -9,4 +9,25 @@ buttons.forEach(
                 );
         });
 }
-)
+);
+const options = {
+root: null, 
+rootMargin: '0px', 
+threshold: 0.1 
+};
+
+const handleIntersect = (entries, observer) => {
+entries.forEach(entry => {
+if (entry.isIntersecting) {
+entry.target.classList.add('square-animation');
+observer.unobserve(entry.target); 
+}
+});
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+const elements = document.querySelectorAll('.offer');
+elements.forEach(element => {
+observer.observe(element);
+});
